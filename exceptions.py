cfd2227859f3fa900cb5e69239029b5ee01b5d93
Amelia Avalon <aphymi@gmail.com>
2018-03-17 14:56:36 -0400

    Create certain exceptions for control flow + clean up manage.py

---
+++ exceptions.py
@@ -0,0 +1,27 @@
+"""
+Definitions for various useful exceptions.
+"""
+
+
+class CommandException(Exception):
+	"""
+	Exception to be raised when there is some user error when attempting to use a command.
+	"""
+	
+	pass
+
+
+class BotRestartException(Exception):
+	"""
+	Exception to be raised in order to restart the bot.
+	"""
+	
+	pass
+
+
+class BotShutdownException(Exception):
+	"""
+	Exception to be raised in order to make the bot shut down.
+	"""
+	
+	pass
--- manage.py
+++ manage.py
@@ -3,11 +3,14 @@ import logging
 import os
 import sys
 
+import config_handler
+from exceptions import BotRestartException, BotShutdownException
+# TODO Figure out a better way to specify implementation to use.
 # Select chat module to use.
-from imps.terminal import bot as chat_imp
+from imps.terminal import bot as bot_imp
 
 if __name__ == "__main__":
-	usage = "Usage: ./manage.py <run|kill|keep|make-configs>"
+	usage = "Usage: manage.py <run|kill|keep|make-configs>"
 	
 	# sys.argv will look something like ['./manage.py', 'start'],
 	#   so the specified command should be at sys.argv[1]
@@ -20,23 +23,40 @@ if __name__ == "__main__":
 		# Run the bot, but don't detach from it.
 		#   This way the running terminal will still get sysout.
 		if command == "keep":
-			chat_imp.run_bot()
+			# TODO Properly configure logging.
+			logging.basicConfig(filename="pb.log", level="DEBUG")
+			
+			while True: # Loop to allow continued running if an exception arises.
+				try:
+					bot = bot_imp.bot()
+					bot.run()
+				
+				except BotRestartException:
+					logging.info("Bot ordered to restart.")
+					continue
+				
+				except BotShutdownException:
+					logging.info("Bot ordered to shut down.")
+					break
+				
+				except Exception:
+					logging.exception()
+					break
 		
 		# Run the bot and detach from it immediately.
 		elif command == "run":
-			# TODO Properly configure logging. Put this somewhere else?
-			logging.basicConfig(filename="pb.log", level="DEBUG")
-			
-			os.system("./manage.py keep &") #TODO Change this to a subprocess call (creationflags=DETACHED_PROCESS)
+			os.system("./manage.py keep &") # TODO Change this to a subprocess call (creationflags=DETACHED_PROCESS)
 		
 		# Stop any running instances of the bot.
 		# This should only be used if the bot won't respond to commands.
-		elif command == "kill":
-			os.system("kill $(ps aux | grep '[m]anage.py runkeep' | awk '{print $2}')")
+		elif command == "kill": # TODO Add a y/n confirmation and warning to kill
+			# TODO Figure out whether this should be run or keep
+			os.system("kill $(ps aux | grep '[m]anage.py run' | awk '{print $2}')")
 		
 		elif command == "make-configs":
 			# TODO Move configs to configs/
 			pass
+		
 		
 		else:
 			print("Unknown command.")
