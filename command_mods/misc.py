9b8b4d0ff3806282039edddca8037e0c77ba766b
Amelia Avalon <aphymi@gmail.com>
2018-10-27 14:39:21 -0400

    Clean up a few misc commands.

--- command_mods/misc.py
+++ command_mods/misc.py
@@ -13,31 +13,33 @@ from plugins.commands import Command, CommandException
 
 MAX_ROLL_AMOUNT = 20  # The maximum number of dice that can be rolled in one call to roll(), to prevent spam/slowing.
 MAX_COMPLEX_SIDES = 999 # Maximum sides for a complex roll, to prevent spam.
-mult_roll_re = re.compile(r"^(?P<arg1>\d+)(d(?P<cmplx_sides>\d+)(?P<modifier>[+-]\d+)?)?$")
+mult_roll_re = re.compile(r"^(?P<amount>\d+)d(?P<sides>\d+)(?P<modifier>[+-]\d+)?$")
 
-# Args get joined in order to allow spaces.
-@Command(args_val=(lambda *args: mult_roll_re.match("".join(args))),
+@Command(args_val=(lambda *args: (len(args) == 1 and args[0].isdigit()) or mult_roll_re.match("".join(args))),
 		 args_usage="<sides>|<amount>d<sides>[<+|-><mod>]")
 def roll(*args):
 	"""
 	Roll some dice.
 	
-	In a simple roll, there is only a single argument, one integer. <sides>
+	In a simple roll, there is only a single argument, one integer which is the number of sides.
 	In a complex roll, multiple dice are being rolled, possibly with a modifier.
 	"""
 	
-	args = mult_roll_re.match("".join(args)).groupdict()
+	# Args get joined in order to allow spaces, e.g. '2 d8'.
+	# Has a side effect of joining numbers separated by a space, but that's fine.
+	args = "".join(args)
+	simple = args.isdigit()
 	
 	# If the second argument isn't included, it's a simple roll.
-	simple = args["cmplx_sides"] is None
 	
 	if simple:
-		sides = int(args["arg1"])
+		sides = int(args)
 		amount = 1
 		mod = 0
 	else:
-		sides = int(args["cmplx_sides"])
-		amount = int(args["arg1"])
+		args = mult_roll_re.match(args).groupdict()
+		sides = int(args["sides"])
+		amount = int(args["amount"])
 		mod = 0 if not args["modifier"] else int(args["modifier"])
 	
 	# Argument errors.
@@ -69,8 +71,13 @@ def roll(*args):
 	
 	return result
 
-@Command(cooldown=5, args_val=(lambda *args: len(args) > 1), args_usage="<choice1> <choice2> [choice3]...")
+
+@Command(args_val=(lambda *args: len(args) > 1), args_usage="<choice1> <choice2> [choice3]...")
 def choose(*args):
+	"""
+	Randomly choose one of the arguments.
+	"""
+	
 	return "My choice is: " + random.choice(args)
 
 
