9db781034d71e9dc0d3273c4680d302478cba9ce
BuildTools <unconfigured@null.spigotmc.org>
2018-12-21 20:12:27 -0700

    Add command to trigger regex responses regardless of trigger permissions.

---
+++ plugins/command_plugins/regex.py
@@ -0,0 +1,22 @@
+from copy import copy
+
+from exceptions import CommandException
+from plugins.commands import Command
+from plugins.regex import regex_msg_handler
+
+@Command(args_val=(lambda *args: args), args_usage="<message>",
+         pass_msg=True)
+def regex(msg, *args):
+	msg = copy(msg)
+	msg.text_content = " ".join(args)
+	msg.sender_group = "default" # TODO Make this not an awful hack.
+	
+	resp = regex_msg_handler(msg, None)
+	
+	if not resp:
+		raise CommandException("No regex found.")
+
+	return resp
+
+
+
